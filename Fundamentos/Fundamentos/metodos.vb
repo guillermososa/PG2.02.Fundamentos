9cbb729816d7fe635008cddd632fae028599eba5
guillermososa <nbkguille@hotmail.com>
2017-06-16 14:10:34 -0300

    Actualizar

---
+++ Fundamentos/Fundamentos/metodos.vb
@@ -0,0 +1,53 @@
+﻿Module metodos
+    Sub main()
+        ' PROBANDO METODOS
+        holaMundo()
+        escribir("Quiero escribir esto")
+        imprimirMaximo(10, 16)
+        escribir(calcularMaximo(20, 15))
+        Dim valor As Double = 30
+        Console.WriteLine("Valor:" & valor)
+        pasoPorValor(valor)
+        Console.WriteLine("Valor otra vez:" & valor)
+        escribir(concatenar("hola", "Como te va?"))
+        escribir(concatenar("hola", "Como te va?", "todo bien?"))
+        Console.ReadKey()
+    End Sub
+
+    Private Function concatenar(valor1 As String, valor2 As String) As String
+        Return valor1 & " " & valor2
+    End Function
+
+    Private Function concatenar(valor1 As String, valor2 As String, valor3 As String) As String
+        Return valor1 & " " & valor2 & " " & valor3
+    End Function
+
+    Private Sub holaMundo()
+        Console.WriteLine("Hola Mundo")
+    End Sub
+
+    Private Sub escribir(cadena As String)
+        Console.WriteLine(cadena)
+    End Sub
+
+    Private Sub pasoPorValor(ByRef dato As Single)
+        dato = dato + 10
+        Console.WriteLine("Valor mas 10:" & dato)
+    End Sub
+
+    Private Sub imprimirMaximo(valor1 As Single, valor2 As Single)
+        If valor1 > valor2 Then
+            escribir(valor1)
+        Else
+            escribir(valor2)
+        End If
+    End Sub
+
+    Private Function calcularMaximo(valor1 As Single, valor2 As Single) As Single
+        If valor1 > valor2 Then
+            Return valor1
+        Else
+            Return valor2
+        End If
+    End Function
+End Module
---
+++ Fundamentos/Fundamentos/tablahash.vb
@@ -0,0 +1,23 @@
+﻿Module tablahash
+    Sub main()
+        ' crea e instancia
+        Dim tablita As New Hashtable
+        ' agrega elementos
+        tablita.Add("R", "Rojo")
+        tablita.Add("V", "Verde")
+        tablita.Add("A", "Azul")
+        ' itera sobre la colección
+        ' cada elemento es cread como un dictionaryentry
+        mostrarElementos(tablita)
+        Console.WriteLine(tablita.ContainsKey("R"))
+        Console.WriteLine(tablita.ContainsKey("W"))
+        tablita.Remove("V")
+        mostrarElementos(tablita)
+    End Sub
+
+    Private Sub mostrarElementos(tablita As Hashtable)
+        For Each elemento As DictionaryEntry In tablita
+            Console.WriteLine(elemento.Key & " " & elemento.Value)
+        Next
+    End Sub
+End Module
